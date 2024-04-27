df4579c5cbf74f458b74b7fc4c1383dd93c99635
matheus-orby <128550445+matheus-orby@users.noreply.github.com>
2024-04-27 12:53:59 -0700

    outline

---
+++ generate_ad.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from PIL import Image
+
+API_KEY = "ADD YOUR API KEY"
+client = OpenAI(api_key=API_KEY)
+
+
+def main():
+  # TODO: Input image and text to generate description of ad, caption and hashtags
+  # TODO: Generate ad image from description
+  # TODO: Upload into shared drive
+  pass
+
+
+if __name__ == "__main__":
+  main()
